194749713eeb359385fc3e43f77f88b1523bcde6
Son Nguyen <95949597+sonnguyen1510@users.noreply.github.com>
2023-12-29 09:08:51 +0700

    29-12

--- MAUILibrary/Platforms/MacCatalyst/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace UILibrary
-{
-    // All the code in this file is only included on Mac Catalyst.
-    public class PlatformClass1
-    {
-    }
-}
--- MAUILibrary/Platforms/Windows/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace UILibrary
-{
-    // All the code in this file is only included on Windows.
-    public class PlatformClass1
-    {
-    }
-}
